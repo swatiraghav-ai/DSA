50e59d986994be8577971ea742456dd3df63ea55
Swati Raghav <swatiraghav771@gmail.com>
2026-02-15 22:24:05 +0530

    initial message

---
+++ insertionsort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    int key, j;
+    
+    for(int i = 1; i < n; i++)
+    {
+        key = arr[i];      // element to be inserted
+        j = i - 1;
+
+        // Move elements greater than key one position ahead
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;  // place key at correct position
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
