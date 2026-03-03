ff20ddcb543b76431f62aa8af3858f49f90968a9
Swati Raghav <swatiraghav771@gmail.com>
2026-03-03 23:05:03 +0530

    initial 4

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -60,5 +60,69 @@ int main() {
         cout << triplet[0] << " " << triplet[1] << " " << triplet[2] << endl;
     }
     
+    return 0;
+}
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> result;
+    int n = nums.size();
+    
+    if(n < 4) return result;
+    
+    sort(nums.begin(), nums.end());
+    
+    for(int i = 0; i < n - 3; i++) {
+        // Skip duplicates for i
+        if(i > 0 && nums[i] == nums[i - 1]) continue;
+        
+        for(int j = i + 1; j < n - 2; j++) {
+            // Skip duplicates for j
+            if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+            
+            int left = j + 1;
+            int right = n - 1;
+            
+            while(left < right) {
+                long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                
+                if(sum == target) {
+                    result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                    
+                    // Skip duplicates
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                }
+                else if(sum < target) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+    }
+    
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1, 0, -1, 0, -2, 2};
+    int target = 0;
+
+    vector<vector<int>> ans = fourSum(nums, target);
+
+    for(auto &quad : ans) {
+        for(int x : quad) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
