6c56313eaaeea038cabeab4ae82c00aea9b1a5cc
Swati Raghav <swatiraghav771@gmail.com>
2026-03-06 23:20:57 +0530

    initial row

---
+++ reverse_row_matrix.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows and columns: ";
+    cin >> rows >> cols;
+
+    int matrix[100][100];
+
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    // Reverse each row
+    for(int i = 0; i < rows; i++) {
+        int start = 0, end = cols - 1;
+        while(start < end) {
+            int temp = matrix[i][start];
+            matrix[i][start] = matrix[i][end];
+            matrix[i][end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    cout << "Matrix after reversing each row:\n";
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
