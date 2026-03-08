d3dcf80e6b5d54652ec52b7fc2735e41570c0cd2
Swati Raghav <swatiraghav771@gmail.com>
2026-03-08 23:22:30 +0530

    initial vect

--- vector_in2D.cpp
+++ vector_in2D.cpp
@@ -1,9 +1,35 @@
-#include <iostream>
+//Declaring a 2D Vector
+/*#include <iostream>
 #include <vector>
 using namespace std;
 
 int main() {
     vector<vector<int>> arr;
+
+    return 0;
+}
+    */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<vector<int>> arr(3, vector<int>(4));
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 4; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Matrix is:\n";
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 4; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
