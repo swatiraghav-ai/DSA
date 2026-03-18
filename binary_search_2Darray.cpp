cb396ab424f4317c305d3ed209c79ef7b8509791
Swati Raghav <swatiraghav771@gmail.com>
2026-03-18 22:18:13 +0530

    initial dfbh

--- binary_search_2Darray.cpp
+++ binary_search_2Darray.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 bool searchMatrix(int matrix[][100], int row, int col, int target) {
@@ -34,6 +34,46 @@ int main() {
         cout << "Element found";
     else
         cout << "Element not found";
+
+    return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+
+bool searchMatrix(int mat[][100], int n, int m, int target) {
+    int i = 0;
+    int j = m - 1;  // start from top-right
+
+    while (i < n && j >= 0) {
+        if (mat[i][j] == target) {
+            cout << "Element found at (" << i << ", " << j << ")";
+            return true;
+        }
+        else if (mat[i][j] > target) {
+            j--;  // move left
+        }
+        else {
+            i++;  // move down
+        }
+    }
+
+    cout << "Element not found";
+    return false;
+}
+
+int main() {
+    int mat[100][100] = {
+        {1, 4, 7, 11},
+        {2, 5, 8, 12},
+        {3, 6, 9, 16},
+        {10,13,14,17}
+    };
+
+    int n = 4, m = 4;
+    int target = 9;
+
+    searchMatrix(mat, n, m, target);
 
     return 0;
 }
