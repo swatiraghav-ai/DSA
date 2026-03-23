906fcae20811fb3869159a00f91205ceeeb59642
Swati Raghav <swatiraghav771@gmail.com>
2026-03-23 22:42:07 +0530

    initial njfv

--- binary_search.cpp
+++ binary_search.cpp
@@ -27,6 +27,19 @@ int main (){
          else 
          end = mid -1;
     } 
+    start = 0 ; end = n-1;
+    while(start<= end){
+        mid = start + (end-start)/2;
+        if(arr[mid]==target){
+            last= mid;
+            start = mid +1;
+        }
+         else  if(arr[mid] < target){
+            start = mid +1;
+         }
+         else 
+         end = mid -1;
+    } 
     cout << "first position " <<first<<endl;
     cout << "last position " << last<<endl;
     return 0 ;
