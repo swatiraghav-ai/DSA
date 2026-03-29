4dd6ff9ffa341bf3aa4218fbe519153810d56635
Swati Raghav <swatiraghav771@gmail.com>
2026-03-30 00:01:37 +0530

    initial dvsdg

--- mapHashing.cpp
+++ mapHashing.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <map>
+#include <algorithm>
 using namespace std;
 int main (){
-    int n ;
+    /*int n ;
     cout << "enter size of an array ";
     cin >> n;
 int arr[n] ;
@@ -24,6 +25,41 @@ for (int i=0;i<n;i++){
             cout <<mpp[num]<< endl;
         }
     }
+}*/
+int n ;
+cout << "enter array  size ";
+cin >> n;
+int arr[n];
+cout << "enter array element ";
+for (int i=0;i<n;i++){
+cin >> arr[i];
 }
+//pre computr 
+map<int , int >mpp;
+for (int i=0;i<n;i++){
+    mpp[arr[i]]++;
+}
+int max_freq=0;
+int min_freq=1e9;
+for(auto it :mpp){
+max_freq=max(max_freq,it.second);
+min_freq=min(min_freq,it.second);
+    cout<< it.first<<"--->"<< it.second  << endl;
+}
+cout << max_freq<<" "<< min_freq<<endl;
+
+int q ;
+cout <<" enter query number";
+cin >> q;
+while (q--){
+    for (int i =0;i<q;i++){
+        int num ;
+        cin >> num;
+        //fetch
+        cout<<mpp[num]<<endl;
+        
+    }
+}
+
 return 0;
 }
