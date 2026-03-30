4ece2b5e83f178583e0d6617094beafcbeae93bb
Swati Raghav <swatiraghav771@gmail.com>
2026-03-30 19:25:43 +0530

    initial cdc

--- array01.cpp
+++ array01.cpp
@@ -10,6 +10,7 @@ cout <<"enter array element";
 for (int i =0;i<n;i++){
     cin >> arr[i];
 }
+/*
 int largest =0;
 for (int i =0;i<n;i++){
     if (arr[i] >largest){
@@ -17,5 +18,17 @@ for (int i =0;i<n;i++){
     }
 }
 cout << largest<< endl ;
+*/
+// second largest element in an array
+int largest =0, second_largest=-1;
+for (int i=0;i<n;i++){
+    if (arr[i]>largest){
+        largest=arr[i];
+    }
+    if(arr[i]>second_largest&& arr[i]!=largest){
+        second_largest=arr[i];
+    }
+}
+ cout<< second_largest<<endl;
 return 0;
 }
