e3aea5f477fbe40724119a6bfb01cc095280f765
Swati Raghav <swatiraghav771@gmail.com>
2026-03-31 23:15:52 +0530

    initial fdfh

--- array02.cpp
+++ array02.cpp
@@ -9,7 +9,7 @@ int main (){
     for(int i=0;i<n;i++){
         cin >>arr[i];
     }
-    // left rotate the array by 1 place 
+    /*// left rotate the array by 1 place 
     int temp =arr[0];
     for (int i =1;i<n;i++){
         arr[i-1]=arr[i];
@@ -19,5 +19,24 @@ arr[n-1]=temp;
 for (int i=0;i<n;i++){
     cout<< arr[i]<<endl;
 }
+    */
+   int d ;
+   cout <<"enetr d";
+   cin >> d;
+d=d%n;
+int temp [d];
+for(int i=0;i<d;i++){
+    temp[i]=arr[i];
+}
+for (int i=d;i<n;i++){
+    arr[i-d]=arr[i];
+}
+for(int i=n-d;i<n;i++){
+    arr[i]=temp[i-(n-d)];
+}
+for (int i=0;i<n;i++){
+    cout<< arr[i] << endl;
+}
+   
 return 0;
 }
