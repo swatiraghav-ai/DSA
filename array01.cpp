a1535c032c82ce857e13c5d0fe202c0e9eeea1d4
Swati Raghav <swatiraghav771@gmail.com>
2026-04-05 23:59:43 +0530

    Update array01.cpp

--- array01.cpp
+++ array01.cpp
@@ -14,7 +14,7 @@ for (int i =0;i<n;i++){
 int largest =arr[0];
 for (int i =0;i<n;i++){
     if (arr[i] >largest){
-        largest = arr[i];
+        largest = arr[i];   
     }
 }
 cout << largest<< endl ;
