64e57ac624456c665075bde7b7ac5976eb741ecc
Swati Raghav <swatiraghav771@gmail.com>
2026-04-07 00:13:50 +0530

    initial dchbcd

--- rearrangeArrayElementBySign.cpp
+++ rearrangeArrayElementBySign.cpp
@@ -11,7 +11,7 @@ int main (){
         cin >> arr[i];
     }
     vector<int>ans(n);
-    int positiveindex=0,negativeindex=1;
+   /* int positiveindex=0,negativeindex=1;
     for (int i=0;i<n;i++){
         if(arr[i]<0){
 ans[negativeindex]=arr[i];
@@ -25,5 +25,44 @@ negativeindex +=2;
     for (int i=0;i<n;i++){
         cout << ans[i] <<" ";
     }
+        */
+
+        // 2nd variety of same question 
+        // in this the positive number and negative number are not same 
+        vector<int>pos,neg;
+        for (int i=0;i<n;i++){
+            if(arr[i] >0){
+pos.push_back(arr[i]);
+            }
+            else{
+                neg.push_back(arr[i]);
+            }
+        }
+        if(pos.size()>neg.size()){
+            for (int i=0;i<neg.size();i++){
+                arr[2*i]=pos[i];
+                arr[2*i+1]=neg[i];
+            }
+            int index =neg.size()*2;
+            for (int i=neg.size();i<pos.size();i++){
+arr[index]=pos[i];
+index++;
+            }
+        }
+        else {
+            for (int i=0;i<pos.size();i++){
+                arr[2*i]=pos[i];
+                arr[2*i+1]=neg[i];
+            }
+                 int index =pos.size()*2;
+                     for (int i=pos.size();i<neg.size();i++){
+                         arr[index]=neg[i];
+                      index++;
+            } 
+
+        }
+        for (int i=0;i<n;i++){
+            cout << arr[i]<< " ";
+        }
     return 0;
 }
