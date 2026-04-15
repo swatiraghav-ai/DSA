580701ab9a38e677cfe37bec7b55343eeaf6394d
Swati Raghav <swatiraghav771@gmail.com>
2026-04-16 00:10:53 +0530

    initial jhdbccd

--- pascal_triangle.cpp
+++ pascal_triangle.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+// element at rth row and column c in pascal triangle 
 int nCr(int n , int r){
     long long res=1;
     for(int i=0;i<r;i++){
@@ -9,13 +10,32 @@ int nCr(int n , int r){
     return res;
 }
 int main() {
-    int r, c;
-    cout << "Enter row and column: ";
-    cin >> r >> c;
+   // int r, c;
+   // cout << "Enter row and column: ";
+   // cin >> r >> c;
 
-   int res= nCr(r-1,c-1);
+   //int res= nCr(r-1,c-1);
 
-    cout << "Element is: " << res;
+   // cout << "Element is: " << res;
 
+
+
+
+
+
+
+
+
+   //print the Nth row of pascal triangle
+   int n;
+   cout <<"enter roe number :";
+   cin >> n;
+   int ans =1;
+  // cout << ans<< " ";
+   for (int i=1;i<n;i++){
+    ans=ans* (n-i);
+    ans=ans/i;
+    cout << ans<< " ";
+   }
     return 0;
 }
