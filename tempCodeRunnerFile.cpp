fd11d30e74655bd2afdaedc4bc75925288e1c53e
swati raghav <swatiraghav771@gmail.com>
2026-06-21 14:40:15 +0530

    added count inversion code

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,16 @@
-while (query--){
-    int num;
-    cin >> num;
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"enter array size";
+    cin>>n;
+    int arr[n];
+    cout<<"enter array element";
+    for (int i=0;i<n;i++){
+         cin>>arr[i];
+    }
+    mergeSort(arr, 0, n - 1);
+cout <<cnt;
+
+    return 0;
+}
