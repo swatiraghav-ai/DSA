ccff3d15b64f23172dd621063618d9e9e7e19a08
swati raghav <swatiraghav771@gmail.com>
2026-06-24 10:20:32 +0530

    adding search element in a roated sorted array code

---
+++ SearchElement_In_a_RoatedSortedArray.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+int SearchElement(vector<int>arr,int n , int target){
+    int low=0, high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[low]<=arr[mid]){
+            if(arr[low]<=target && target <=arr[mid]){
+            high=mid-1;
+            }
+            else 
+            low=mid+1;
+        }
+        else
+        if(arr[mid]<=target && target<=arr[high]){
+            low=mid+1;
+        }
+        else 
+        high=mid-1;
+    }
+    return -1;
+}
+int main (){
+    int n;
+    cout<< "enter array size";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"enter array element";
+    for(int i=0;i<n;i++){
+        cin>> arr[i];
+    }
+    int target;
+    cout << "enter target";
+    cin >> target;
+    int ans = SearchElement(arr, n , target);
+    cout<< ans;
+    return 0;
+}
